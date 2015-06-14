df2d8546153b1e94260c6e81f0a9fd82b6749b3b
Ben Mathew <dbm0204@gmail.com>
2015-06-14 13:11:30 +0530

    <Replaced Button Object with imageView>

---
+++ MyApplication/app/src/main/java/com/example/ben/myapplication/Router.java
@@ -0,0 +1,7 @@
+package com.example.ben.myapplication;
+
+/**
+ * Created by Ben on 6/13/2015.
+ */
+public class Router {
+}
---
+++ MyApplication/app/src/main/java/com/example/ben/myapplication/node.java
@@ -0,0 +1,19 @@
+package com.example.ben.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ben on 6/13/2015.
+ */
+public class node  {
+
+    private String mNodeName;
+    ImageView image;
+    List <Router> myList;
+
+}
